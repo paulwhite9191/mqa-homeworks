7edfa1caeb6a5310d7852c1ff336e27586cac4d4
Paul White <paratrak@list.ru>
2023-09-16 17:36:22 +0300

    Code fix

--- 2.1 auto/hw_app/app/src/androidTest/java/com/netology/tabbedapplication/MainActivityTest.java
+++ 2.1 auto/hw_app/app/src/androidTest/java/com/netology/tabbedapplication/MainActivityTest.java
@@ -38,46 +38,15 @@ public class MainActivityTest {
     @Test
     public void mainActivityTest() {
         ViewInteraction textView = onView(
-                allOf(withId(R.id.section_label), withText("Page: 1"),
-                        withParent(allOf(withId(R.id.constraintLayout),
-                                withParent(withId(R.id.view_pager)))),
-                        isDisplayed()));
+                allOf(withId(R.id.section_label), withText("Page: 1"), isDisplayed()));
         textView.check(matches(withText("Page: 1")));
 
         ViewInteraction tabView = onView(
-                allOf(withContentDescription("Tab 2"),
-                        childAtPosition(
-                                childAtPosition(
-                                        withId(R.id.tabs),
-                                        0),
-                                1),
-                        isDisplayed()));
+                allOf(withContentDescription("Tab 2"), isDisplayed()));
         tabView.perform(click());
 
         ViewInteraction textView2 = onView(
-                allOf(withId(R.id.section_label), withText("Page: 2"),
-                        withParent(allOf(withId(R.id.constraintLayout),
-                                withParent(withId(R.id.view_pager)))),
-                        isDisplayed()));
+                allOf(withId(R.id.section_label), withText("Page: 2"), isDisplayed()));
         textView2.check(matches(withText("Page: 2")));
-    }
-
-    private static Matcher<View> childAtPosition(
-            final Matcher<View> parentMatcher, final int position) {
-
-        return new TypeSafeMatcher<View>() {
-            @Override
-            public void describeTo(Description description) {
-                description.appendText("Child at position " + position + " in parent ");
-                parentMatcher.describeTo(description);
-            }
-
-            @Override
-            public boolean matchesSafely(View view) {
-                ViewParent parent = view.getParent();
-                return parent instanceof ViewGroup && parentMatcher.matches(parent)
-                        && view.equals(((ViewGroup) parent).getChildAt(position));
-            }
-        };
     }
 }
